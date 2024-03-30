21f40d13d1c30fa939be5ad13633e46b73651aeb
karthik-tenali <63164669+karthik-tenali@users.noreply.github.com>
2024-03-30 12:39:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 const clock = () => {
   const date = new Date();
-  const hours = date.getHours();
+  let hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
 
-  let ses = hours > 12 ? (session = "PM") : (session = "AM");
+  let session = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12; // Convert to 12-hour format and handle midnight (0 -> 12)
 
-  let h = hours > 12 ? `${0}${hours - 12}` : `${0}${hours}`;
-  if (h == 00) {
-    h = 12;
-  }
+  let h = hours < 10 ? "0" + hours : hours; // Add leading zero if hours is less than 10
   let m = minutes < 10 ? "0" + minutes : minutes;
   let s = seconds < 10 ? "0" + seconds : seconds;
 
   document.querySelector(".hours").innerHTML = h;
   document.querySelector(".minutes").innerHTML = m;
   document.querySelector(".seconds").innerHTML = s;
-  document.querySelector(".session").innerHTML = ses;
+  document.querySelector(".session").innerHTML = session;
 };
 
 setInterval(clock, 1000);
